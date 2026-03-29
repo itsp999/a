4585b0a0c67cb5fd1a0fbc41f8e61e33f1c61036
183 <183241677@qq.com>
2026-03-29 17:14:49 +0800

    Fix Vercel build script: download Flutter SDK during build

---
+++ frontend/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Clone Flutter into a temporary directory
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable
+export PATH="$PATH:$(pwd)/flutter/bin"
+
+# Disable analytics to speed up
+flutter config --no-analytics
+flutter config --enable-web
+
+# Build the project
+flutter build web --release
